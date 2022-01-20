c7111ed7368e6ffece827c9c3c1274c58112c631
Lucas Esteves <lucas.esteves.rocha@gmail.com>
2022-01-20 19:40:45 +0100

    Target file for ridope

---
+++ target/build/terasic_de10lite/software/include/generated/variables.mak
@@ -0,0 +1,25 @@
+PACKAGES=libc libcompiler_rt libbase libfatfs liblitespi liblitedram libliteeth liblitesdcard liblitesata bios
+PACKAGE_DIRS=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libc /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libcompiler_rt /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libbase /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libfatfs /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/liblitespi /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/liblitedram /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libliteeth /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/liblitesdcard /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/liblitesata /mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/bios
+LIBS=libc libcompiler_rt libbase libfatfs liblitespi liblitedram libliteeth liblitesdcard liblitesata
+TRIPLE=riscv64-unknown-elf
+CPU=vexriscv
+CPUFAMILY=riscv
+CPUFLAGS=-march=rv32im     -mabi=ilp32 -D__vexriscv__
+CPUENDIANNESS=little
+CLANG=0
+CPU_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/cores/cpu/vexriscv
+SOC_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc
+PICOLIBC_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/pythondata-software-picolibc/pythondata_software_picolibc/data
+COMPILER_RT_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/pythondata-software-compiler_rt/pythondata_software_compiler_rt/data
+export BUILDINC_DIRECTORY
+BUILDINC_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/target/build/terasic_de10lite/software/include
+LIBC_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libc
+LIBCOMPILER_RT_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libcompiler_rt
+LIBBASE_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libbase
+LIBFATFS_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libfatfs
+LIBLITESPI_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/liblitespi
+LIBLITEDRAM_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/liblitedram
+LIBLITEETH_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/libliteeth
+LIBLITESDCARD_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/liblitesdcard
+LIBLITESATA_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/liblitesata
+BIOS_DIRECTORY=/mnt/c/Users/lucas/Desktop/2022.1/InnovR/image-ridope/litex-inst/litex/litex/soc/software/bios
---
+++ target/soc_ridope.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# This file is part of LiteX-Boards.
+# Copyright (c) 2019 msloniewski <marcin.sloniewski@gmail.com>
+# Modified 2021 by mpelcat <mpelcat@insa-rennes.fr>
+# Modified 2022 by lesteves <lesteves@insa-rennes.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import os
+import argparse
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex_boards.platforms import de10lite # referencing the platform
+
+from litex.soc.cores.clock import Max10PLL
+from litex.soc.integration.soc import SoCRegion
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+
+class _CRG(Module): # Clock Region definition
+    def __init__(self, platform, sys_clk_freq):
+        self.rst = Signal()
+        self.clock_domains.cd_sys = ClockDomain()
+
+        clk50 = platform.request("clk50")
+
+        # PLL - instanciating an Intel FPGA PLL outputing a clock at sys_clk_freq
+        self.submodules.pll = pll = Max10PLL(speedgrade="-7")
+        self.comb += pll.reset.eq(self.rst)
+        pll.register_clkin(clk50, 50e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
+
+class BaseSoC(SoCCore): # SoC definition - memory sizes are overloaded
+    def __init__(self, sys_clk_freq=int(50e6), with_video_terminal=False, **kwargs):
+        platform = de10lite.Platform()
+
+        # SoCCore ----------------------------------------------------------------------------------
+        #These kwargs overwrite the value find on soc / soc_core
+        #So you can change here the sizes of the different memories
+        kwargs["integrated_rom_size"] = 0x8000 # chose rom size, holding bootloader (min = 0x6000)
+        kwargs["integrated_sram_size"] = 0x2000 # chose sram size, holding stack and heap. (min = 0x6000)
+        kwargs["integrated_main_ram_size"] = 0x8000 # 0 means external RAM is used, non 0 allocates main RAM internally
+
+        SoCCore.__init__(self, platform, sys_clk_freq,
+            ident          = "LiteX SoC on DE10-Lite",
+            ident_version  = True,
+            **kwargs)
+
+        self.submodules.crg = _CRG(platform, sys_clk_freq) # CRG instanciation
+
+def main(): # Instanciating the SoC and options
+    parser = argparse.ArgumentParser(description="LiteX SoC on DE10-Lite")
+    parser.add_argument("--build",               action="store_true", help="Build bitstream")
+    parser.add_argument("--load",                action="store_true", help="Load bitstream")
+    parser.add_argument("--sys-clk-freq",        default=50e6,        help="System clock frequency (default: 50MHz)")
+    parser.add_argument("--with-video-terminal", action="store_true", help="Enable Video Terminal (VGA)")
+    builder_args(parser)
+    soc_core_args(parser)
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        sys_clk_freq        = int(float(args.sys_clk_freq)),
+        with_video_terminal = args.with_video_terminal,
+        **soc_core_argdict(args)
+    )
+    builder = Builder(soc, **builder_argdict(args))
+    builder.build(run=args.build)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".sof"))
+
+if __name__ == "__main__":
+    main()
